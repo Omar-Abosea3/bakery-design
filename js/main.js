8d0f5d3db30e0fca32b11bdac51570f55f7abaa1
Omar Ashraf <124902211+Omar-Abosea3@users.noreply.github.com>
2023-02-10 16:04:17 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 var myNav = document.getElementById('mynav');
+var prevScrollpos = window.pageYOffset;
 window.onscroll = function () { 
     if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200 ) {
         myNav.classList.add("nav-colored");
@@ -8,4 +9,11 @@ window.onscroll = function () {
         myNav.classList.add("bg-transparent");
         myNav.classList.remove("nav-colored");
     }
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.querySelector(".navbar1").style.top = "0";
+    } else {
+        document.querySelector(".navbar1").style.top = "-70px";
+    }
+    prevScrollpos = currentScrollPos;
 };
