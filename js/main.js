0cdd4b1f0581883c60200e08a68dfe011ac078e2
Omar Ashraf <124902211+Omar-Abosea3@users.noreply.github.com>
2023-03-15 18:42:39 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 var myNav = document.getElementById('mynav');
+var prevScrollpos = window.pageYOffset;
 window.onscroll = function () { 
     if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200 ) {
         myNav.classList.add("nav-colored");
@@ -8,4 +9,45 @@ window.onscroll = function () {
         myNav.classList.add("bg-transparent");
         myNav.classList.remove("nav-colored");
     }
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.querySelector(".navbar1").style.top = "0";
+    } else {
+        document.querySelector(".navbar1").style.top = "-100%";
+    }
+    prevScrollpos = currentScrollPos;
+
+    if($(window).scrollTop() >= $('#About').offset().top){
+        $('.toTop').removeClass('d-none');
+    }else{
+        $('.toTop').addClass('d-none');
+    }
+    if($('html,body').scrollTop() >= $('#Home').offset().top){
+        $('nav ul a').css('color','#8E7754');
+        $('nav ul a').not($('nav ul a').eq(0)).css('color','white')
+    }if($('html,body').scrollTop()>=$('#About').offset().top){
+        $('nav ul a').css('color','#8E7754');
+        $('nav ul a').not($('nav ul a').eq(1)).css('color','white')
+    }if($('html,body').scrollTop()>=$('#OurOffer').offset().top){
+        $('nav ul a').css('color','#8E7754');
+        $('nav ul a').not($('nav ul a').eq(2)).css('color','white')
+    }if($('html,body').scrollTop()>=$('#Gallary').offset().top){
+        $('nav ul a').css('color','#8E7754');
+        $('nav ul a').not($('nav ul a').eq(3)).css('color','white')
+    }if($('html,body').scrollTop()>=$('#Blog').offset().top){
+        $('nav ul a').css('color','#8E7754');
+        $('nav ul a').not($('nav ul a').eq(4)).css('color','white')
+    }if($('html,body').scrollTop()>=$('#Contact').offset().top){
+        $('nav ul a').css('color','#8E7754');
+        $('nav ul a').not($('nav ul a').eq(5)).css('color','white')
+    }
 };
+
+$('nav ul a').click(function(){
+    let sectionOffsetTop=$($(this).attr('href')).offset().top;
+    $('html,body').animate({scrollTop:sectionOffsetTop},2000);
+ })
+
+$('.toTop').click(function(){
+    $('html,body').animate({scrollTop:0},2000);
+ })
